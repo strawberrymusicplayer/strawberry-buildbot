08193b40a2364909dad472b654ab066b7990e446
Jonas Kvinge <jonas@jkvinge.net>
2019-09-20 00:40:31 +0200

    Make sure icu4c is compiled first

--- config/master/strawberry/builders.py
+++ config/master/strawberry/builders.py
@@ -582,6 +582,16 @@ def MakeMXEBuilder():
     shell.Compile(
       name="compile",
       workdir="source",
+      command=["make", "-j", MAKE_JOBS, "icu4c" ],
+      timeout=108000,
+      haltOnFailure=True,
+    )
+  )
+
+  f.addStep(
+    shell.Compile(
+      name="compile",
+      workdir="source",
       command=["make", "-j", MAKE_JOBS],
       timeout=108000,
       haltOnFailure=True,
