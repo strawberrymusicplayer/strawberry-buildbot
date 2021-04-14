8c326f87383db23536328da2b07675e0832e8cf6
Jonas Kvinge <jonas@jkvinge.net>
2021-04-14 16:55:01 +0200

    Include gst-discoverer-1.0.exe in windows setup

--- config/master/strawberry/builders.py
+++ config/master/strawberry/builders.py
@@ -641,6 +641,7 @@ def MakeWindowsBuilder(is_debug, is_64, with_qt6):
     "killproc.exe",
     "gdb.exe",
     "gst-launch-1.0.exe",
+    "gst-discoverer-1.0.exe",
     "libfreetype-6.dll",
     ("libssl-1_1-x64.dll" if is_64 else "libssl-1_1.dll"),
     ("libcrypto-1_1-x64.dll" if is_64 else "libcrypto-1_1.dll"),
