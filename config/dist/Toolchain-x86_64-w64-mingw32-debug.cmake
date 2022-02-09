01b8abcaa3cafc90b8aa7cbbaaed6b19c4a3e6a7
Jonas Kvinge <jonas@jkvinge.net>
2022-02-09 19:27:42 +0100

    Update cmake toolchain files

---
+++ config/dist/Toolchain-x86_64-w64-mingw32-debug.cmake
@@ -0,0 +1,8 @@
+SET(CMAKE_SYSTEM_NAME Windows)
+SET(CMAKE_C_COMPILER /persistent-data/mingw/mxe-debug/source/usr/bin/x86_64-w64-mingw32.shared-gcc)
+SET(CMAKE_CXX_COMPILER /persistent-data/mingw/mxe-debug/source/usr/bin/x86_64-w64-mingw32.shared-g++)
+SET(CMAKE_RC_COMPILER /persistent-data/mingw/mxe-debug/source/usr/bin/x86_64-w64-mingw32.shared-windres)
+SET(CMAKE_FIND_ROOT_PATH /persistent-data/mingw/mxe-debug/source/usr/x86_64-w64-mingw32.shared/)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
