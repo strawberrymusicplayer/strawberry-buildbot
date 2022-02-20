935540113ea2cb958eaf839b559dbe4c4c6fdcb4
Jonas Kvinge <jonas@jkvinge.net>
2022-02-20 04:18:19 +0100

    Update clean-mxe-build.sh

--- clean-mxe-build.sh
+++ clean-mxe-build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo docker-compose run worker-mingw make -C /persistent-data/mingw/mxe/source clean
+sudo docker-compose run worker-mingw make -C /persistent-data/mingw/mxe-debug/source clean
+sudo docker-compose run worker-mingw make -C /persistent-data/mingw/mxe-release/source clean
