6e9ee4be68f6831a737b4252fd49aed6a8dfe97a
Jonas Kvinge <jonas@jkvinge.net>
2023-04-28 21:42:16 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,3 +4,4 @@ sudo docker-compose down
 sudo docker rm $(sudo docker ps -aq)
 sudo docker rmi $(sudo docker images -q)
 sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
+sudo docker system prune -a -f
